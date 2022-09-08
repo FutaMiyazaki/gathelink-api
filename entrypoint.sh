7b4b8f4885988a28a175816d103427513ed1a253
capcap-jp <miyamiyacap@gmail.com>
2022-09-08 23:15:38 +0900

    🚀 ECS にデプロイするための設定を追加

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,10 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
-# 2回目以降のデプロイ時には下記の行をコメントアウトする
-#bundle exec rails db:create RAILS_ENV=production
+#下記行は2回目以降のデプロイ時にコメントアウトする
+#rails db:create RAILS_ENV=production
 
-#bundle exec rails db:migrate RAILS_ENV=production
+rails db:migrate RAILS_ENV=production
 
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
