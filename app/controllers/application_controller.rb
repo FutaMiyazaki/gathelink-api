34b35d01cea63b65ac94fb6e0b367e1f2f2b8b56
capcap-jp <miyamiyacap@gmail.com>
2022-09-12 17:26:24 +0900

    ✨ devise による認証機能を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+  include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  before_action do
+    I18n.locale = :ja
+  end
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
+  end
 end
