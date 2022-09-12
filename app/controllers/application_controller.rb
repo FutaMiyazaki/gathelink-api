8b3817adadc0a0c4c3c3da65a83040d851ae8653
capcap-jp <miyamiyacap@gmail.com>
2022-09-12 18:18:42 +0900

    🚨 Fix linter warnings

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,7 @@ class ApplicationController < ActionController::API
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :image])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :image])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name image])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name image])
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ module Myapp
     config.load_defaults 7.0
 
     config.session_store :cookie_store, key: '_interslice_session'
-      config.middleware.use ActionDispatch::Cookies
-      config.middleware.use config.session_store, config.session_options
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use config.session_store, config.session_options
 
     # Configuration for the application, engines, and railties goes here.
     #
