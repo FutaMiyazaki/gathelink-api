18ab3e03062b8469081700c1acb6467c33711c97
capcap-jp <miyamiyacap@gmail.com>
2022-10-29 19:52:24 +0900

    Refactoring folder_favorites#destroy

--- app/controllers/api/v1/folder_favorites_controller.rb
+++ app/controllers/api/v1/folder_favorites_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::FolderFavoritesController < ApplicationController
   before_action :authenticate_api_v1_user!
-  before_action :set_folder
+  before_action :set_favorite, only: %i[destroy]
+  before_action :correct_user, only: %i[destroy]
 
   def create
     favorite = FolderFavorite.new(folder_id: params[:folder_id], user_id: current_api_v1_user.id)
@@ -14,8 +15,7 @@ class Api::V1::FolderFavoritesController < ApplicationController
   end
 
   def destroy
-    favorite = current_api_v1_user.folder_favorites.find_by!(folder_id: params[:folder_id])
-    if favorite.destroy
+    if @favorite.destroy
       render status: :no_content,
              json: current_api_v1_user.as_json(include: [{ favorite_folders: { expect: %i[created_at updated_at] } }],
                                                only: %i[id])
@@ -30,11 +30,11 @@ class Api::V1::FolderFavoritesController < ApplicationController
     params.permit(:folder_id)
   end
 
-  def set_folder
-    @folder = Folder.find(params[:folder_id])
-    if @folder.id.to_s != params[:folder_id]
-      render status: :forbidden,
-             json: { message: "不正なリクエストです" }
-    end
+  def set_favorite
+    @favorite = FolderFavorite.find(params[:id])
+  end
+
+  def correct_user
+    render status: :forbidden, json: { message: "不正なリクエストです" } if current_api_v1_user.id != @favorite.user_id
   end
 end
