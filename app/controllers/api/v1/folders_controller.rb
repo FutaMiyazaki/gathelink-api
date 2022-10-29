1e6068e174523aec5c674c1c709ba57024f99dea
capcap-jp <miyamiyacap@gmail.com>
2022-10-29 19:52:49 +0900

    レスポンス値にお気に入り情報を追加

--- app/controllers/api/v1/folders_controller.rb
+++ app/controllers/api/v1/folders_controller.rb
@@ -13,7 +13,8 @@ class Api::V1::FoldersController < ApplicationController
   def show
     folder = Folder.find(params[:id])
     render status: :ok, json: folder.as_json(include: [{ user: { only: %i[id name email] } },
-                                                       { links: { expect: %i[user_id] } }])
+                                                       { links: { expect: %i[user_id] } },
+                                                       { folder_favorites: { only: %i[id user_id] } }])
   end
 
   def create
